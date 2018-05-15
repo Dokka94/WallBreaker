d78163beaf4d0d17b1a96687eea5107d9559fe6c
Kalcsevszki Regina <kalcsevszkiregi@gmail.com>
2018-05-15 21:19:18 +0200

    Kezdőmenüben megadható a név amin elmenti a játékot BUG: második pályától kezdve nagyon lelassul!!!

--- MenuController.py
+++ MenuController.py
@@ -15,6 +15,7 @@ class MenuController():
 
         self.Screen = pygame.display.set_mode((Constants.Screen_Width, Constants.Screen_Height))
         self.wallBreaker = Wallbreaker(self.Screen)
+        self.givenName = ""
         
            
     
@@ -28,24 +29,45 @@ class MenuController():
         all_sprite_list.add(self.newButton)
         all_sprite_list.add(self.loadButton)
         all_sprite_list.add(self.htpButton)
-        
+
+        font = pygame.font.Font(None, 25)
+        # text_score = font.render("Score: ", True, self.Black)
+        text = font.render("Your name: ", True, Constants.Black)
+        text_name = font.render(self.givenName, True, Constants.Black)
+
         intro=True
         
         while intro:
+
+            text_name = font.render(self.givenName, True, Constants.Black)
+
             for event in pygame.event.get():
                 if event.type==pygame.QUIT:
                     pygame.quit()
                     quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_BACKSPACE:
+                        self.givenName = self.givenName[0:len(self.givenName)-1]
+                    elif len(self.givenName) < 4:
+                        self.givenName += pygame.key.name(event.key).upper()
                 
             self.Screen.fill(Constants.White)
             self.Screen.blit(intro_im,(60,10))
-            
+
+            self.Screen.blit(text, [Constants.Screen_Width/2-85, 550])
+            displayname = self.givenName
+            morecharcount = 4 - len(self.givenName)
+            for c in range(0, morecharcount):
+                displayname += '_'
+            text_name = font.render(displayname, True, Constants.Black)
+            self.Screen.blit(text_name, [Constants.Screen_Width/2 + 25, 550])
+
             all_sprite_list.draw(self.Screen)
             
             all_sprite_list.update()         
             
             if self.newButton.click():
-                self.wallBreaker.newProcess(50,11,3)
+                self.wallBreaker.newProcess(self.givenName)
             if self.loadButton.click():
                 self.wallBreaker.loadProcess()
                 
